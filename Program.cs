9dc09d95c1561a244ad409143066e3b94762d9fe
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-09-19 20:47:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fundamentals_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
